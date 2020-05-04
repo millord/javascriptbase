53de018b6db26cc8f29bcdf450993742c70dc487
millord <juancarlosmillord@gmail.com>
2020-05-04 19:43:37 -0400

    first commit

---
+++ .next/static/webpack/static/development/pages/index.js.bfb497894936261f8675.hot-update.js
@@ -0,0 +1,51 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/intro.js":
+/*!*****************************!*\
+  !*** ./components/intro.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Intro; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _lib_constants__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../lib/constants */ "./lib/constants.js");
+var _jsxFileName = "/home/juanmillord/Desktop/hard-parts/next/myblog/components/intro.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+function Intro() {
+  return __jsx("section", {
+    className: "flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12",
+    __self: this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 5
+    }
+  }, __jsx("h1", {
+    className: "text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8",
+    __self: this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6,
+      columnNumber: 7
+    }
+  }, "Juan Carlos Millord's Blog."), __jsx("h4", {
+    className: "text-center md:text-left text-lg mt-5 md:pl-8",
+    __self: this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9,
+      columnNumber: 7
+    }
+  }, "Blog tecnol\xF3gico sobre JavaScript, React y Geo aplicaciones"));
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.bfb497894936261f8675.hot-update.js.map
